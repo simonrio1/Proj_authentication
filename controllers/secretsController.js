7769e969e6284856ee3844561a3cfd755cbfb290
simonrio1 <simon.rio411@gmail.com>
2023-03-31 18:04:04 +0200

    passport working

--- controllers/secretsController.js
+++ controllers/secretsController.js
@@ -1,4 +1,4 @@
-const secretsServices = require("../services/secretsServices");
+// const secretsServices = require("../services/secretsServices");
 const User = require("../models/user");
 const passport = require("passport");
 // const md5 = require("md5"); Lvl 3
@@ -10,11 +10,21 @@ exports.getHomePage = (req, res) => {
 }
 
 exports.getRegisterPage = (req, res) => {
-    res.render("register");
+    if (req.isAuthenticated()) {
+        console.log("Already authenticated");
+        res.redirect("/secrets");
+    } else {
+        res.render("register");
+    }
 }
 
 exports.getLoginPage = (req, res) => {
-    res.render("login");
+    if (req.isAuthenticated()) {
+        console.log("Already authenticated");
+        res.redirect("/secrets");
+    } else {
+        res.render("login");
+    }
 }
 
 exports.getSecretsPage = (req, res) => {
@@ -33,66 +43,47 @@ exports.getSubmitPage = (req, res) => {
 }
 
 exports.logout = (req, res) => {
-    req.logout(function(err) {
-        if (err) { return next(err); }
-        res.redirect('/');
-      });
+    req.logout(function (err) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.redirect('/');
+        }
+    });
 }
 
 exports.register = async (req, res) => {
-    console.log(req.body.username);
     User.register({ username: req.body.username }, req.body.password, (err) => {
         if (err) {
             console.log('error while user register!', err);
             res.redirect("/register");
         } else {
             console.log('user registered!');
-            passport.authenticate("local",{ failureRedirect: '/login', failureMessage: true })(req, res, () => {
+            passport.authenticate("local", { failureRedirect: '/login', failureMessage: true })(req, res, () => {
                 console.log(req.user);
                 res.redirect('/secrets');
             });
-            
+
         }
     });
 }
 
 exports.login = (req, res) => {
-    // try {
-
-    //     const email = req.body.username;
-
-    //     const user = await secretsServices.checkUser(email);
-
-    //     if (user) {
-    //         const match = await bcrypt.compare(req.body.password, user.password);
-    //         if (match) {
-    //             res.redirect("/secrets");
-    //         } else {
-    //             console.log("wrong password");
-    //             res.redirect("/login");
-    //         }
-    //     } else {
-    //         console.log("unknown email address");
-    //         res.redirect("/login");
-    //     }
-    // } catch (err) {
-    //     res.json(err);
-    // }
     const user = new User({
         username: req.body.username,
         password: req.body.password
     });
-    req.login(user, function(err) {
+    req.login(user, function (err) {
         if (err) {
             console.log(err);
-            res.redirect('/login'); 
+            res.redirect('/login');
         } else {
-            passport.authenticate('local',{ failureRedirect: '/login', failureMessage: true })(req, res, () => {
+            passport.authenticate('local', { failureRedirect: '/login', failureMessage: true })(req, res, () => {
                 console.log(req.user);
                 res.redirect('/secrets');
             });
         }
-      });
+    });
 }
 
 exports.submitSecret = (req, res) => {
